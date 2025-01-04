f17e958512bd282de3a89b1be0204118b2eca28e
LathaGurusamy <lathalearningwithguvi@gmail.com>
2025-01-04 23:52:35 +0530

    ff

--- src/main/java/SeleniumGoal/SeleniumPractiseAndExrecise/TwentySixthTaskThree.java
+++ src/main/java/SeleniumGoal/SeleniumPractiseAndExrecise/TwentySixthTaskThree.java
@@ -1,4 +1,4 @@
-package SeleniumGoal.SeleniumPractiseAndExercise;
+package SeleniumGoal.SeleniumPractiseAndExrecise;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
