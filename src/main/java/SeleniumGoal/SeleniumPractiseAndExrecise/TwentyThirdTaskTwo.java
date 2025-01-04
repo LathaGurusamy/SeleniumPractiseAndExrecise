c3ba1f354fff975ee0a329d09ef9eb4bdc1c99ff
LathaGurusamy <lathalearningwithguvi@gmail.com>
2025-01-04 16:36:49 +0530

    sun

--- src/main/java/SeleniumGoal/SeleniumPractiseAndExrecise/TwentyThirdTaskTwo.java
+++ src/main/java/SeleniumGoal/SeleniumPractiseAndExrecise/TwentyThirdTaskTwo.java
@@ -13,47 +13,37 @@ package SeleniumGoal.SeleniumPractiseAndExrecise;
 
 		public class TwentyThirdTaskTwo {
 		    public static void main(String[] args) {
-		        // Set up the ChromeDriver using WebDriverManager
 		        WebDriverManager.chromedriver().setup();
 
-		        // Initialize WebDriver
 		        WebDriver driver = new ChromeDriver();
 
-		        // Open jQueryUI droppable demo site
 		        driver.get("https://jqueryui.com/droppable/");
 
-		        // Maximize the window
 		        driver.manage().window().maximize();
 		        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10)); // Implicit wait
 
-		        // Switch to the iframe that contains the droppable elements
 		        driver.switchTo().frame(driver.findElement(By.className("demo-frame")));
 		        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10)); // Implicit wait
 
 
-		        // Find the source element
 		        WebElement sourceElement = driver.findElement(By.id("draggable"));
 		        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10)); // Implicit wait
 
 
-		        // Find the target element
 		        WebElement targetElement = driver.findElement(By.id("droppable"));
 		        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10)); // Implicit wait
 
 
-		        // Perform drag and drop operation
 		        Actions action = new Actions(driver);
 		        action.dragAndDrop(sourceElement, targetElement).perform();
 		        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10)); // Implicit wait
 
 
-		        // Verify the color change after drop
 		        String color = targetElement.getCssValue("background-color");
 		        System.out.println("Color after drop: " + color);
 		        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10)); // Implicit wait
 
 
-		        // Verify the text change after drop
 		        String text = targetElement.getText();
 		        System.out.println("Text after drop: " + text);
 		        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10)); // Implicit wait
@@ -65,7 +55,6 @@ package SeleniumGoal.SeleniumPractiseAndExrecise;
 		            System.out.println("Drag and drop operation failed!");
 		        }
 
-		        // Close the browser
 		        driver.quit();
 		    }
 		}
